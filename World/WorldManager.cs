00d5b469c3aea6deefd25d3f6e3ec8e90c885cf6
Redmancometh <Brendanstcurry@gmail.com>
2016-09-28 15:57:45 -0500

    Block stuff.

--- World/WorldManager.cs
+++ World/WorldManager.cs
@@ -6,13 +6,12 @@ using Colonist;
 public class WorldManager
 {
     private Dictionary<string, Colonist.World> worldMap = new Dictionary<string, Colonist.World>();
-    private BlockingQueue<World> worldProcessQueue = new BlockingQueue<World>();
+    private Queue<World> worldProcessQueue = new Queue<World>();
         
     public void processWorlds()
     {
 
         Colonist.World world;
-        worldProcessQueue.TryDequeue(out world);
     }
 
     public void processWorld(Colonist.World world)
